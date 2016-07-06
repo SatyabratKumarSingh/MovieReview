e2f0914a6395f3a4569afe341a3b1c29ccbded6f
sudhansusingh22 <sudhansusingh22@gmail.com>
2016-07-05 21:14:51 -0400

    youtube api oauth 2.0

--- youtube/app.js
+++ youtube/app.js
@@ -1,42 +1,87 @@
+"use strict";
+
 var youtubeAPI = require("youtube-api")
   , YConfig = require("./config").youtube
   , FireConfig = require("./config").fbase;
-  
+ 
+const fs = require("fs")
+    , readJson = require("r-json")
+    , Lien = require("lien")
+    , Logger = require("bug-killer")
+    , opn = require("opn")
+    , prettyBytes = require("pretty-bytes")
+    ;
+    
+const CREDENTIALS = readJson(`${__dirname}/credentials.json`);
+let server = new Lien({
+    host: "localhost"
+  , port: 5000
+});
+
+let oauth = youtubeAPI.authenticate({
+    type: "oauth"
+  , client_id: CREDENTIALS.web.client_id
+  , client_secret: CREDENTIALS.web.client_secret
+  , redirect_url: CREDENTIALS.web.redirect_uris[0]
+});
+
+opn(oauth.generateAuthUrl({
+    access_type: "offline"
+  , scope: ["https://www.googleapis.com/auth/youtube"]
+}));
+// oauth.setCredentials("ya29.CjAXA_r_zlk58QjcN3g9C3hcHKhSwjyIPa4Y1vCL7AuJAQoB8cWxCzkVftA9_jCLwKI");
+
+var Youtube = {
+    searchFunctions: require('./lib/search-functions'),
+};
+
+
+
+server.addPage("/oauth2callback", lien => {
+    Logger.log("Trying to get the token using the following code: " + lien.query.code);
+    oauth.getToken(lien.query.code, (err, tokens) => {
+
+        if (err) {
+            lien.lien(err, 400);
+            return Logger.log(err);
+        }
+
+        Logger.log("Got the tokens.");
+		console.log(tokens);
+        oauth.setCredentials(tokens);
+		getYoutubeData();
+        lien.end("The video is being uploaded. Check out the logs in the terminal.");
+
+    });
+});
+
 var async = require('async');
 var firebase = require('firebase');
 
-firebase.initializeApp({
-serviceAccount: {
-    projectId:   FireConfig.projectId,
-    clientEmail: FireConfig.clientEmail,
-    privateKey:  FireConfig.privateKey
-  },
-  databaseURL: FireConfig.kDBBaseRef
-});
-
-var db = firebase.database();
 
 
-var postResults = [];
-var userResults = [];
-var postDict = {};
-var imageDict = {};
-var userArray = [];
 
-var postKeys = ["id", "title","url", "thumbnail", "createdDate"];
-	console.log(YConfig.API);
+// firebase.initializeApp({
+// serviceAccount: {
+//     projectId:   FireConfig.projectId,
+//     clientEmail: FireConfig.clientEmail,
+//     privateKey:  FireConfig.privateKey
+//   },
+//   databaseURL: FireConfig.kDBBaseRef
+// });
+// 
+// var db = firebase.database();
+// 
+// 
+// var postResults = [];
+// var userResults = [];
+// var postDict = {};
+// var imageDict = {};
+// var userArray = [];
+// 
+// var postKeys = ["id", "title","url", "thumbnail", "createdDate"];
+// 	console.log(YConfig.API);
 
-var Youtube = {
-    _APIKEY: YConfig.API,
-      setup: function (apiKEY) {
-        this._APIKEY = apiKEY;
-        youtubeAPI.authenticate({
-            type: 'key',
-            key: apiKEY
-        });
-    },
-    searchFunctions: require('./lib/search-functions'),
-};
 
 function getYoutubeData(){
    async.waterfall([
@@ -62,128 +107,7 @@ function createLog(msg){
 	log['time'] = startTime;
 	return log;
 }
-/*
-function saveData(result){
-	postResults = result[0];
-	userResults = result[1];
-	postResults.forEach(function(json){
-		var post = {};
-		var postValue = [];
 
-		var pId = json.id;
-// 		var pName = json.name;
-		var pUrl = json.link;
-		var pMessage = json.message;
-		var pCreatedTime = Date.parse(json.created_time)/1000;
-		var pUpdatedTime = Date.parse(json.updated_time)/1000;
-		var priceAndLoc = getPriceAndLocAndName(pMessage);
-		var pPrice = priceAndLoc[1];
-		var pLoc = priceAndLoc[2];
-		var pName = priceAndLoc[0];
-		var uId = json.from.id;
-		var uName = json.from.name;
-
-		var imgIdAndSrcArray = getImageIdAndUrl(json);
-		var imgSrcArray = imgIdAndSrcArray[1];
-		var pImages = {};
-		imgIdAndSrcArray[0].forEach(function (imgId){
-			pImages[imgId] = true;
-		});
-		var users = {};
-		var userValues = [];
-		userValues.push(uId);
-		userValues.push(uName);
-		userValues.push(pId);
-		userValues.push(userResults[uId].data.url);
-		for(var i=0; i<userKeys.length;i++){
-			users[userKeys[i]] = userValues[i];
-		}
-		postValue.push(users);
-		postValue.push(pName);
-		postValue.push(pMessage);
-		postValue.push(pUrl);
-		postValue.push(pUpdatedTime);
-		postValue.push(pCreatedTime);
-		postValue.push(pPrice);
-		postValue.push(pLoc);
-		postValue.push(pImages);
-
-		for(var i=0; i<postKeys.length;i++){
-			if(typeof postValue[i] == 'undefined'){
-				post[postKeys[i]] = '';
-			}
-			else{
-				post[postKeys[i]] = postValue[i];
-			}
-		}
-		postDict[pId] = post;
-
-		// creating user records
-		/*var users = {};
-		var userValues = [];
-		userValues.push(uId);
-		userValues.push(uName);
-		userValues.push(pId);
-		userValues.push(userResults[uId].data.url);
-		for(var i=0; i<userKeys.length;i++){
-			users[userKeys[i]] = userValues[i];
-		}
-		userArray.push(users);
-		*/
-		// creating image record
-// 		for(var i=0; i<imgIdAndSrcArray[0].length;i++){
-
-/*		var c = 0;
-		imgIdAndSrcArray[0].forEach(function (imgId){
-			var image = {};
-			var imageValue = [];
-// 			imageValue.push(imgId);
-			imageValue.push(imgSrcArray[c]);
-			imageValue.push(uId);
-			imageValue.push(pId);
-			for(var i=0; i<imageKeys.length;i++){
-				image[imageKeys[i]] = imageValue[i];
-			}
-			c = c+1;
-			imageDict[imgId] = image;
-		});
-
-	});
-*/
-	// saving data to firebase
-/*
-	async.series
-    ([
-        function (callback)
-        {
-        	saveToDatabaseWithRef(FireConfig.kDBPostRef,postDict);
-            callback();
-        }
-        ,
-        function (callback)
-        {
-        	saveToDatabaseWithRef(FireConfig.kDBImageRef,imageDict);
-            callback();
-        }
-        ,
-        function (callback)
-        {
-        	saveUserAndImages(FireConfig.kDBImageRef,imageArray);
-            callback();
-        }
-    ]
-    ,
-    function(err)
-    {
-    	if(err != null){
-    		saveLogs(FireConfig.kDBLogRef,createLog(err));
-    	}
-		saveLogs(FireConfig.kDBLogRef,createLog('saving data'));
-        console.log("Done !");
-    });
-}
-
-*/
 var onComplete = function(error) {
   if (error) {
     if(error != null){
@@ -220,4 +144,4 @@ function saveLogs(childRef, data){
 // 	});
 // }
 
-getYoutubeData();
+
