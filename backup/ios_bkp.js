9175b71c241419bbdce59e380103cedfcefb8bb9
sudhansusingh22 <sudhansusingh22@gmail.com>
2016-07-28 01:42:08 -0400

    changes for grid view

---
+++ backup/ios_bkp.js
@@ -0,0 +1,11 @@
+ /*
+                <ListView
+                     ref="listview"
+                     renderSeparator={this.renderSeparator}
+                     dataSource={this.state.dataSource}
+                     renderRow={this.renderRow}
+                     automaticallyAdjustContentInsets={false}
+                     keyboardDismissMode="on-drag"
+                     keyboardShouldPersistTaps={true}
+                     showsVerticalScrollIndicator={false}
+                /> */
